f370dff6e414d391bcf27cb9c27c9d4bf21f337f
nao1412 <naoya_greeeen_0720@icloud.com>
2021-06-16 16:58:59 +0900

    d

---
+++ src/scripts/sample.js
@@ -0,0 +1,6 @@
+// test = [
+//   { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
+//   { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+// ];
+
+console.log('Hello, world');
--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -13,12 +13,16 @@ function buildFullName(data) {
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
-    a = [
-        {id: 1, full_name: '大木 優', first_name: '優', family_name: '大木',affilication: 'TechTrain', is_student: false},
-        {id: 2, full_name: '山田 太郎', first_name: '太郎', family_name: '山田',affilication: 'HogeHoge大学', is_student: true}
-     ]
-    return a
+    full_name = familly_name + ' ' + first_name
+    data.full_name = full_name
+    return 
 }
+
+const test = [
+    { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
+    { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+];
+return test.map(buildFullName)
 
 // 出力が想定される答え
 // const test = [
